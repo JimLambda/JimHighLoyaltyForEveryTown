bf9dec1318c8b535d547115d0f56b4c96286b863
SuperJim <526554176@qq.com>
2022-12-28 09:43:27 +0900

    Add project files.

---
+++ JimHighLoyaltyForEveryTownBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.CampaignSystem.LogEntries;
+using TaleWorlds.CampaignSystem.SceneInformationPopupTypes;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.ComponentInterfaces;
+
+namespace JimHighLoyaltyForEveryTown
+{
+    internal class JimHighLoyaltyForEveryTownBehavior : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.DailyTickSettlementEvent.AddNonSerializedListener(this, DailyTickSettlementFunction);
+        }
+
+        private void DailyTickSettlementFunction(Settlement settlement)
+        {
+            // Your code for doing something involving a settlement.
+            try
+            {
+                if (settlement.IsTown || settlement.IsCastle)
+                {
+                    settlement.Town.Loyalty += 100;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                InformationManager.DisplayMessage(new InformationMessage(ex.Message));
+            }
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+    }
+}
---
+++ MySubModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace JimHighLoyaltyForEveryTown
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            if (gameStarterObject == null)
+                return;
+
+            if (game.GameType is Campaign)
+            {
+                //The current game is a campaign
+                CampaignGameStarter campaignGameStarter = gameStarterObject as CampaignGameStarter;
+                campaignGameStarter.AddBehavior(new JimHighLoyaltyForEveryTownBehavior());
+            }
+
+        }
+    }
+}
